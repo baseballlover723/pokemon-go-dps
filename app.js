8e9c320f1b5d6ebc7014df1d41e9b1578391e2a6
Philip Ross <rosspa@rose-hulman.edu>
2017-03-14 02:38:52 -0400

    readded google analytics

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.expose(app.settings.env, "env");
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
